350e7c8b31e19f7ed88845904e9eba582b99ffda
thienphuprogrammer <thienphuprogrammer@gmail.com>
2024-10-17 17:07:14 +0700

    # Model

--- src/training/scripts/train.py
+++ src/training/scripts/train.py
@@ -1,12 +1,45 @@
+import configparser
+
 import numpy as np
 
 from src.data_pipeline.loaders.loaders import load_har_dataset
 from src.models.model import *
-from src.training.scripts.utils import *
+from src.training.scripts.utils import visualize_loss, visualize_accuracy
 
-dataset_path = "./../data/raw/HAR/"
-resize_dataset_video_path = "./../data/processed/HAR/"
-output_csv_path = "./../data/processed/HAR/UCF"
+# Read hyperparameters from the config file .yml
+config = configparser.ConfigParser()
+config.read('./config/config.yml')
+
+# Set the paths
+dataset_path = config['PATHS']['dataset_path']
+resize_dataset_video_path = config['PATHS']['resize_dataset_video_path']
+output_csv_path = config['PATHS']['output_csv_path']
+
+hidden_size = int(config['HYPERPARAMETERS']['hidden_size'])
+lstm_layer = int(config['HYPERPARAMETERS']['lstm_layer'])
+patch_size = int(config['HYPERPARAMETERS']['patch_size'])
+num_classes = int(config['HYPERPARAMETERS']['num_classes'])
+num_epochs = int(config['HYPERPARAMETERS']['num_epochs'])
+batch_size = int(config['HYPERPARAMETERS']['batch_size'])
+learning_rate = float(config['HYPERPARAMETERS']['learning_rate'])
+number_block = int(config['HYPERPARAMETERS']['number_block'])
+device = config['HYPERPARAMETERS']['device']
+
+# dataset_path = "./../data/raw/HAR/"
+# resize_dataset_video_path = "./../data/processed/HAR/"
+# output_csv_path = "./../data/processed/HAR/UCF"
+#
+# # Set hyperparameters
+# hidden_size = 128
+# lstm_layer = 35
+# patch_size = 225
+# num_classes = 6
+# num_epochs = 100
+# batch_size = 64
+# learning_rate = 0.001
+# number_block = 1
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 
 x_train, y_train = load_har_dataset(dataset_path,
                                     resize_dataset_video_path,
@@ -36,17 +69,6 @@ x_val = torch.from_numpy(x_val).float()
 y_val = torch.from_numpy(y_val).float()
 print(f"Validation data shape: {x_val.shape}, Validation labels shape: {y_val.shape}")
 print(f"Validation data type: {x_val.dtype}, Validation labels type: {y_val.dtype}")
-
-# Set hyperparameters
-hidden_size = 128
-lstm_layer = 35
-patch_size = 225
-num_classes = 6
-num_epochs = 100
-batch_size = 64
-learning_rate = 0.001
-number_block = 1
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train),
                                            batch_size=batch_size, shuffle=True)
